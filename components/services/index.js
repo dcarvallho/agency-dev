f28011df61dc564a111b44f1baae6a22dde17120
Davi Augusto <davi_augusto11@hotmail.com>
2024-01-05 02:00:06 -0300

    feat: added cards with services in grid

--- components/services/index.js
+++ components/services/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import style from './style.module.scss';
+import { SERVICES_TYPE } from '../../constants';
 
 const Services = () => {
   return (
-    <div>
+    <div className={style.services}>
         <h1>O que fazemos?</h1>
-        <div>
-            <div>isso</div>
-            <div>aquilo</div>
-            <div>aquela</div>
-            <div>aquele</div>
+        <div className={style.servicesCards}>
+            {SERVICES_TYPE.map((types, index) => (
+              <div key={index} className={style.servicesStyleCards}>{types.title}</div>
+            ))}
         </div>
     </div>
   )
--- constants/index.js
+++ constants/index.js
@@ -2,3 +2,12 @@ export const TITLES = {
     title: 'Nós desenvolvemos',
     subtitle: 'Poderosas Experiências'
 }
+
+export const SERVICES_TYPE = [
+    {title: 'Desenvolvimento Full Stack', description: 'lorem ipsum'},
+    {title: 'Sites leves e rápidos', description: 'lorem ipsum'},
+    {title: 'E-commerces', description: 'lorem ipsum'},
+    {title: 'SEO', description: 'lorem ipsum'},
+    {title: 'Estratégias de marketing', description: 'lorem ipsum'},
+    {title: 'Hospedagem', description: 'lorem ipsum'},
+]
